e46e76e78826f50f606543ca3640b4e83c1ae0a3
John Carlo <johncarlolabanero@gmail.com>
2023-04-25 21:00:04 +0800

    Add one tap google sign in

--- app/src/main/java/com/example/diary/Login.java
+++ app/src/main/java/com/example/diary/Login.java
@@ -9,10 +9,14 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
@@ -23,10 +27,15 @@ import com.google.firebase.auth.FirebaseUser;
 public class Login extends AppCompatActivity {
     TextInputEditText editTextEmail, ediTextPassword;
     TextView btnTxtSignUp;
+    ImageView buttonLogGoogle;
     Button buttonLog;
     ProgressBar progressBar;
     FirebaseAuth mAuth;
     Context context;
+//    CredentialsClient mCredentialsClient;
+//
+    SignInClient oneTapClient;
+    BeginSignInRequest signInRequest;
     @Override
     public void onStart() {
         super.onStart();
@@ -43,6 +52,9 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         init();
+        String email, password;
+        email = String.valueOf(editTextEmail.getText());
+        password = String.valueOf(ediTextPassword.getText());
         btnTxtSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,9 +66,6 @@ public class Login extends AppCompatActivity {
         buttonLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email, password;
-                email = String.valueOf(editTextEmail.getText());
-                password = String.valueOf(ediTextPassword.getText());
                 progressBar.setVisibility(View.VISIBLE);
                 if(TextUtils.isEmpty(email)){
                     Toast.makeText(context, "Email empty", Toast.LENGTH_SHORT).show();
@@ -84,14 +93,32 @@ public class Login extends AppCompatActivity {
                 );
             }
         });
+        buttonLogGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                oneTapClient = Identity.getSignInClient(context);
+                signInRequest = BeginSignInRequest.builder()
+                        .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                                .setSupported(true)
+                                // Your server's client ID, not your Android client ID.
+                                .setServerClientId(getString(R.string.default_web_client_id))
+                                // Only show accounts previously used to sign in.
+                                .setFilterByAuthorizedAccounts(true)
+                                .build())
+                        .build();
+            }
+        });
     }
     private void init(){
         context = this;
         editTextEmail = findViewById(R.id.email_login);
         ediTextPassword = findViewById(R.id.password_login);
         buttonLog = findViewById(R.id.btn_login);
+        buttonLogGoogle = findViewById(R.id.btn_loginGoogle);
         progressBar = findViewById(R.id.progress_horizontal_1);
         btnTxtSignUp = findViewById(R.id.SignUp);
         mAuth = FirebaseAuth.getInstance();
+//        mCredentialsApiClient = Credentials.getClient(this);
     }
 }
